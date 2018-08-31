66eaa087b0cbbaa8f3e2c109b2e92fbdfdd44fe7
赵金星 <zhaojinxing@zuoyebang.com>
2018-08-31 17:09:14 +0800

    no message

--- TestDemo/ViewController.m
+++ TestDemo/ViewController.m
@@ -9,33 +9,74 @@
 #import "ViewController.h"
 #import "UIButton+SKButton.h"
 #import "OneViewController.h"
+#import <sys/sysctl.h>
 
 @interface ViewController ()
+{
+    NSInteger _state;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *showView;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)buttonPress:(id)sender {
+
+    CGRect frame = self.showView.frame;
+
+    if (_state%2)
+    {
+        frame = CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 200.f);
+    }
+    else
+    {
+        frame = CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 400.f);
+    }
+
+    [UIView animateWithDuration:0.3 animations:^{
+        self.showView.frame = frame;
+    }];
+
+    _state++;
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-#if __IPHONE_11_0
-    
-    NSLog(@"11");
-    
-#endif
-    
-    NSLog(@"%d", [UIScreen instancesRespondToSelector:@selector(currentMode)]);
+//#if __IPHONE_11_0
+//
+//    NSLog(@"11");
+//
+//#endif
+//
+//    NSLog(@"%d", [UIScreen instancesRespondToSelector:@selector(currentMode)]);
+//
+//
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+//    button.bounds = CGRectMake(0, 0, 100, 50);
+//    button.center = self.view.center;
+//    [button setTitle:@"button_test" forState:UIControlStateNormal];
+//    [button addTarget:self action:@selector(zjxButtonPress:) forControlEvents:UIControlEventTouchUpInside];
+//    [button setNeedsCameraPermission];
+//
+//    [self.view addSubview:button];
 
+    size_t size;
+    sysctlbyname("kern.boottime", NULL, &size, NULL, 0);
 
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.bounds = CGRectMake(0, 0, 100, 50);
-    button.center = self.view.center;
-    [button setTitle:@"button_test" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(zjxButtonPress:) forControlEvents:UIControlEventTouchUpInside];
-    [button setNeedsCameraPermission];
+    char *boot_time = malloc(size);
+    sysctlbyname("kern.boottime", boot_time, &size, NULL, 0);
 
-    [self.view addSubview:button];
+    uint32_t timestamp = 0;
+    memcpy(&timestamp, boot_time, sizeof(uint32_t));
+    free(boot_time);
+
+    NSDate* bootTime = [NSDate dateWithTimeIntervalSince1970:timestamp];
+
+    NSLog(@"bootTime = %@", bootTime);
 
 }
 
--- TestDemo/ZJXModel.m
+++ TestDemo/ZJXModel.m
@@ -19,6 +19,7 @@
         NSLog(@"%s", __func__);
         for (int i = 0; i<10; i++) {
             NSLog(@"这里是live代码");
+        }
         for (int i = 0; i<10; i++) {
             NSLog(@"这里是sell代码");
         }
